e888da418a5ac9613fb165112166aef9f9cf6c50
kevinmukuna <mukuna.kevin@gmail.com>
2021-02-22 10:31:50 +0000

    addec comment and java doc to the both stack and stackElement classes

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -1,19 +1,46 @@
+/**
+ * this is a stack class that delegate a stack elements, which consist of values passed and the next value,
+ * the class pushes new items to the top of stack and
+ * the class pop from the top of the stack, meaning the last entry is the first to come out and first entry is the
+ * last to come out
+ * @param <T> the T stands for type
+ */
 public class Stack<T>{
-
+    /**
+     * @see #size --> this referes to the size of the stack. it initiated to zero upon creation.
+     * @see StackElement --> this holds the stack of whatever object we decide to pass into the array.
+     * @see Stack --> the constructor is used for initialisation of an empty stack.
+     * @see #push(T Object) --> this method takes a value/object of type T and pushes it  ot the top of the stack.
+     * @see #pop() --> pops the items on the top of the stack and the previous item becomes the item on top.
+     * @see #top() --> returns the item on top of the stack, the most recently pushed item.
+     * @see #init() --> this method calls in the constructor which then initialises the stack.
+     * @see #show() --> this method prints each item in a new line, till the stack is empty.
+     * @see #isStackEmpty() --> returns a bool if the stack is empty or not.
+     * @see #errorMesage() --> this is used to return an exception when an unexpected execution is  requested.
+     * @see #getSize() --> returns the size of the stack.
+     */
     private int size;
     StackElement<T> top;
 
-    // stack functional method
+    // ----------------------------stack functional method----------------------------
     public Stack() {
         size = -1;
         top = null;
     }
 
+    /**
+     * this method takes a value/object of type T and pushes it  ot the top of the stack.
+     * @param valuePassed the value being pushed to the top of the stack
+     */
     public void push(T valuePassed) {
         top = new StackElement<>(valuePassed, top);
         size++;
     }
 
+    /**
+     * pops the items on the top of the stack and the previous item becomes the item on top.
+     * @return delete the top of the stack
+     */
     public T pop() {
         StackElement<T> previousTop = top;
         if (isStackEmpty()){
@@ -23,6 +50,10 @@ public class Stack<T>{
         return previousTop.getvalue();
     }
 
+    /**
+     * returns the item on top of the stack, the most recently pushed item.
+     * @return the top of the stack
+     */
     public T top() {
 
         if (isStackEmpty()){
@@ -31,11 +62,19 @@ public class Stack<T>{
         return top.getvalue();
     }
 
+    /**
+     * this method calls in the constructor which then initialises the stack.
+     * initialisation of the stack method
+     */
     public void init() {
         size = 0;
         top = null;
     }
 
+    /**
+     *  this method prints each item in a new line, till the stack is empty.
+     *  makes a copy of the stack and prints out items one by one as long as it has nextvalue
+     */
     public void show() {
         StackElement<T> stackCOpy = top;
         while (stackCOpy.nextValue != null ){
@@ -45,18 +84,32 @@ public class Stack<T>{
         System.out.println(stackCOpy.valuePassed);
     }
 
-    // extra method
+    // ---------------------------extra method------------------------
+
+    /**
+     * returns a bool if the stack is empty or not.
+     * @return bool true or false on whether the stack is empty or not
+     */
     public boolean isStackEmpty() {
         return size <= -1;
     }
 
+    /**
+     * this is used to return an exception when an unexpected execution is  requested
+     * @return an error message
+     */
     public T errorMesage (){
         return (T) "Sorry Stack is Empty";
     }
 
+    /**
+     * returns the size of the stack
+     * @return size of the stack
+     */
     public int getSize() {
         return size;
     }
+
 
     public static void main(String[] args){
 
--- src/main/java/StackElement.java
+++ src/main/java/StackElement.java
@@ -1,6 +1,6 @@
 /**
  * the purpose of this class is to delegate to the element, so as in to able to create multiple types,premitive type
- * without the need to change the whole code over and over again--> this class is generic of type T
+ * without the need to change the whole code over and over again --> this class is generic of type T
  * @Author Kevin Mukuna
  * @param <T>, the type of the value being stored in the stack
  */
@@ -11,8 +11,8 @@ public class StackElement<T> {
 
     /**
      *
-     * @param valuePassed
-     * @param nextValue
+     * @param valuePassed the value being pushed to the top of the stack
+     * @param nextValue the next value to be pushed to teh top of the stack (being a stackelement of T )
      */
     public StackElement(T valuePassed, StackElement<T> nextValue){
         this.valuePassed = valuePassed;
@@ -20,13 +20,18 @@ public class StackElement<T> {
 
     }
 
+    /**
+     * @return the previous value before the current passed value, the value just below the top value of top
+     */
     public StackElement<T> getnext() {
         return nextValue;
     }
 
+    /**
+     * @return the value being passed or the value on top of the stack
+     */
     public T getvalue() {
         return valuePassed;
     }
-
 
 }
