f4a91e356ec7a35263d320e8684888609238be85
kevinmukuna <mukuna.kevin@gmail.com>
2021-02-23 05:46:43 +0000

    failing testingForThePOPFunctionality() method

--- src/test/java/TestStack.java
+++ src/test/java/TestStack.java
@@ -5,24 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestStack {
     Stack<String> moduleCodes = new Stack<>();
 
-//    @Test
-//    @DisplayName("push to the top of stack testing")
-//    void pushToTheTopOfStackTesting() {
-//        moduleCodes.push("CS3305 ");
-//        moduleCodes.push("CS3306 ");
-//        moduleCodes.push("CS3506 ");
-//        moduleCodes.push("CS3506 ");
-//        moduleCodes.push("CS3514 ");
-//        moduleCodes.push("CS3516 ");
-//        moduleCodes.push("CS3519 ");
-//        moduleCodes.push("CS1117 ");
-//
-//        assertAll(
-//
-//                ()->assertTrue()
-//        );
-//    }
-
     @Test
     @DisplayName("testing the init method")
     void testingTheInitMethod() {
@@ -55,5 +37,11 @@ public class TestStack {
         testingForPushMethod();
         System.out.println();
         assertEquals("CS1117", moduleCodes.top());
+    }
+
+    @Test
+    @DisplayName("testing for the POP functionality")
+    void testingForThePOPFunctionality() {
+        fail("not implemented");
     }
 }
