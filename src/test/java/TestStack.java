6dba6edabd6259491016dc3ad2b045d6f4d7436d
kevinmukuna <mukuna.kevin@gmail.com>
2021-02-23 07:28:51 +0000

    comented the TestStack.java class

--- src/test/java/TestStack.java
+++ src/test/java/TestStack.java
@@ -2,8 +2,32 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author kevinmukuna
+ * the following is test class implmented using Junit and maven
+ * this is part of CS3306 assigment 1 by Marc Van Dongen
+ * this modules and assignement was designed to get us familiar with some of the tools and version control used
+ * in work placement fields
+ */
 public class TestStack {
+    /**
+     * This is test class implemented using the TDD approach, where i write the minimum code required for the test case
+     * and then iterate over it till it meets the perfect passing requirement.
+     * @see #testingForPushMethod() : this method is used for testing entries being pushed to the top of the stack,
+     * it returns true if the number of items on the stack eqauls the size of the stack
+     * @see #testingForTopItemOnTopOfTheStack() : this method returns true of the last entry that was pushed is equal
+     * to the item on the top of the stack
+     * @see #testingForThePOPFunctionality() : this method returns true if the item previous to the item on top is
+     * returned as top when the pop method is called.
+     * @see #testingTheInitMethod() this method is used to initiliase the stack
+     * @see #getTheCurrentSizeOfTheStack() returns the size of the stack
+     * @see #testingForTheShowMethod() this method returns everything that was pushed unto the stack
+     * @see #testingToEnsureThatErrorMessageWhenACallDoesNotMeetRequirement() this method returns error message  if an
+     * operation is called on the methods when it is empty
+     * @see #testingForTopAndPopWhenTheStackHasBeenEmptied() this method true if the message returned is as expected
+     */
     Stack<String> moduleCodes = new Stack<>();
+
 
     @Test
     @DisplayName("testing the init method")
