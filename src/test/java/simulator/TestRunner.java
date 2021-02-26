fa48167582d8f6e3469509e42d4e70c948aaeded
kevinmukuna <mukuna.kevin@gmail.com>
2021-02-26 11:17:50 +0000

    added testrunner class, which is used for runing TestStack class

---
+++ src/test/java/simulator/TestRunner.java
@@ -0,0 +1,9 @@
+package simulator;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        TestStack test = new TestStack();
+        System.out.println("---------------Testing each unit using junit -------------");
+        test.main();
+    }
+}
